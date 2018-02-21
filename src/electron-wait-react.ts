5d9c49e176e62c4d257ca9300f64b169135aea9f
jbpark <jbpark@wemakeprice.com>
2018-02-21 13:08:57 +0900

    add procfile & update package.json for electron

--- src/electron-wait-react.ts
+++ src/electron-wait-react.ts
@@ -6,13 +6,15 @@ process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 const client = new net.Socket();
 
 let startedElectron = false;
-const tryConnection = () => client.connect({port: port}, () => {
+const tryConnection = () => client.connect(
+  { port: port },
+  () => {
     client.end();
     if (!startedElectron) {
       console.log('starting electron. ' + process.env.ELECTRON_START_URL);
       startedElectron = true;
       const exec = require('child_process').exec;
-      exec('yarn electron');
+      exec('npm run electron');
     }
   }
 );
